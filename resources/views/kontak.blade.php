200c6d4c6aa590df9f4b8ac82b10f706b8679e9a
Muhamad Rifqi <nanal.setiawan1@gmail.com>
2022-02-23 13:50:58 +0700

    first commit

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('judul_halaman')
+
+@section('konten')
+
+<h2>My Kontak Broww</h2>
+    <p>Nama : Muhamad Rifqi
+    <P>Tinggal : Sumedang Jatinangor </p>
+    <p>No Tlp : 088136469484</p>
+    <p>Instagram : @mhmrifqi</P>
+
+@endsection
---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('judul_halaman')
+
+@section('konten')
+
+    <h2>Tentang aku yang apa adanya</h2>
+    <p>Terimakasih telah memberikan kenangan singkat,namun sangat melekat</p>
+    <p>Sombong = Tambah demage</p>
+
+@endsection
